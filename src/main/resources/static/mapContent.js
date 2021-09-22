eef2d73b0cbe2610b7ea697a2b6be503110cd663
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-09-22 10:12:35 -0700

    need api key

---
+++ src/main/resources/static/mapContent.js
@@ -0,0 +1,11 @@
+console.log("really hope you can see this!!");
+alert("the js is finally working!");
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
