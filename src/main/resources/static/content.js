16fc66d7f2e7059117f4e4da5ae16f8aab83c1db
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-09-29 07:31:55 -0700

    added comments for readability

--- src/main/resources/static/content.js
+++ src/main/resources/static/content.js
@@ -1,28 +1,14 @@
 // map setup
 let map;
 
+// this function is called in the html every time the page loads
 function initMap() {
+
+  // creating the google map
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
-
-    // this div holds the heading of the map page
-    var headingDiv = document.createElement("div")
-    headingDiv.id = "headingDiv"
-
-        // making page title
-          var title = document.createElement("div")
-          title.innerHTML = "<h1>Bumper Stickers!</h1>"
-          headingDiv.appendChild(title);
-
-          // making back to home button
-          var b2h = document.createElement("button")
-          b2h.innerHTML="<p>Back to home!</p>"
-          headingDiv.appendChild(b2h);
-
-    document.getElementById("content").appendChild(headingDiv);
-    headingDiv.style.textAlign = "center";
 
    // this div holds the cards on the map page
     var cardDiv = document.createElement("div")
@@ -31,38 +17,45 @@ function initMap() {
         // this will loop through every sticker in the database
         for(i=0;i<10;i++){
 
-            // creating one card
+            // creating card div
             var card = document.createElement("div")
-            card.class = "card"
+            card.className = "card"
             card.id = "card-"+i
             card.style.width = "18rem"
 
                 // creating photo of the card
                 var photo = document.createElement("img")
-                photo.class = "card-img-top"
+                photo.className = "card-img-top"
                 photo.id = "card-photo-"+i
                 photo.width = "100"
                 photo.src = "https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg" // eventually change to sticker image source
                 card.appendChild(photo)
 
-                // div holding the card body
+                // creating div holding the card body
                 var cardBody = document.createElement("div")
-                cardBody.class = "card-body"
+                cardBody.className = "card-body"
                 cardBody.id = "card-body-"+i
 
                     // creating caption of the card
                     var caption = document.createElement("h5")
-                    caption.class= "card-title"
+                    caption.className = "card-title"
                     caption.id = "card-caption-"+i
                     caption.innerHTML="this is the caption" // eventually change to sticker caption
                     cardBody.appendChild(caption)
 
                     // creating the tags
-                    var tags = document.createElement("p")
-                    tags.class = "card-text"
+                    var tags = document.createElement("h6")
+                    tags.className = "card-text"
                     tags.id = "card-tags-"+i
                     tags.innerHTML="#sticker #dog #sticker" // eventually change to sticker tags
                     cardBody.appendChild(tags)
+
+                    // creating the name
+                    var name = document.createElement("p")
+                    name.className = "card-text"
+                    name.id = "card-name"+i
+                    name.innerHTML="posted by: user" // eventually change to sticker tags
+                    cardBody.appendChild(name)
 
                 card.appendChild(cardBody)
 
