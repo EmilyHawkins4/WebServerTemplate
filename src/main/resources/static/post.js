b9881150a5eccc0fefa130e6dfa80af04686cd36
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-10-27 07:57:14 -0700

    cards

--- src/main/resources/static/post.js
+++ src/main/resources/static/post.js
@@ -4,6 +4,7 @@ let map;
 // making the map
 function initMap() {
     console.log("can u see this?");
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
@@ -11,6 +12,7 @@ function initMap() {
   });
 
   }
+
 
 // this function is executed when the user selects the submit button
   function submitForm(){
@@ -21,8 +23,8 @@ function initMap() {
     // sets form value with id of tags to variable tags
     var tags = document.getElementById("tags").value;
 
-    // sets form value with id of caption to variable caption
-    var caption = document.getElementById("caption").value;
+    // sets form value with id of title to variable title
+    var title = document.getElementById("title").value;
 
     // sets form value with id of img to variable image
     var image = document.getElementById("img").value;
