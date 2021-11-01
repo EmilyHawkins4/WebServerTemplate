edc2c25ebb37cc6b834fb04e6e04ed917f54cf11
giselaroberts <groberts22@holynames-sea.org>
2021-11-01 07:18:39 -0700

    updated SQL w auto increment

---
+++ src/main/java/service/BumperStickerPost.java
@@ -0,0 +1,54 @@
+package service;
+
+public class BumperStickerPost {
+    private String img;
+    private String tg;
+    private double lat;
+    private double lon;
+    //private int PostId;
+    //private int TagId;
+
+    //constructor
+    public BumperStickerPost(String image, String tag, double latitude, double longitude){
+        img = image;
+        tg = tag;
+        lat = latitude;
+        lon = longitude;
+    }
+
+    //get methods
+    public String getImage (){
+        return img;
+    }
+
+    public String getTag (){
+        return tg;
+    }
+
+    public double getLat (){
+        return lat;
+    }
+
+    public double getLong (){
+        return lon;
+    }
+
+    //set methods
+    public void setImage(String newImage){
+       img = newImage;
+    }
+
+    public void setTag (String newTag){
+        tg = newTag;
+    }
+
+    public void setLat (double newLat){
+        lat = newLat;
+    }
+
+    public void setLon (double newLon){
+        lon = newLon;
+    }
+}
+
+
