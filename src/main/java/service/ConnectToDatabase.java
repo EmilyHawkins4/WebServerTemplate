e80a411d286fb74156b379864105ef82265202de
sagestefonic <sstefonic22@holynames-sea.org>
2021-11-22 07:50:37 -0800

    Got displayPosts to work, without Tags

--- src/main/java/service/ConnectToDatabase.java
+++ src/main/java/service/ConnectToDatabase.java
@@ -17,14 +17,14 @@ public class ConnectToDatabase {
             connection = DriverManager.getConnection(url);
 
             String query1 = "SELECT Image, lat, long, title FROM Posts;";
-            String query2 = "SELECT Tags.tagId, TaggedPosts.PostId, Tags.tagname" + "\n" +
+            /*String query2 = "SELECT Tags.tagId, TaggedPosts.PostId, Tags.tagname" + "\n" +
                     "FROM TaggedPosts" + "\n" +
-                    "INNER JOIN Tags ON TaggedPosts.tagId=Tags.tagId;";
+                    "INNER JOIN Tags ON TaggedPosts.tagId=Tags.tagId;";*/
 
             Statement st = connection.createStatement();//create the java statement
 
             ResultSet resultSet1 = st.executeQuery(query1);//execute the query and get a java resultset
-            ResultSet resultSet2 = st.executeQuery(query2);
+            //ResultSet resultSet2 = st.executeQuery(query2);
 
 
             while (resultSet1.next()) {
@@ -47,7 +47,7 @@ public class ConnectToDatabase {
 
 
                 //printing out the results
-                System.out.format(sticker.getImage(), sticker.getTags(), sticker.getLat(), sticker.getLong());
+                System.out.println(sticker.getImage()+ " "+sticker.getTags()+ " "+sticker.getLat()+" "+ sticker.getLong());
             }
 
             connection.close();
