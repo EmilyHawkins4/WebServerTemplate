5d503d5c565562e040016f725b8a9bdf23c0d5ee
kayla <kmattson22@holynames-sea.org>
2021-11-22 07:22:42 -0800

    form submission

--- src/main/java/service/DatabaseConnection.java
+++ src/main/java/service/DatabaseConnection.java
@@ -2,47 +2,48 @@ package service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class databaseConnection {
+public class DatabaseConnection {
 
-    //save data from form to database
-   public void saveData(BumperStickerPost post) {
-        String hostName = "hna.database.windows.net";
-        String dbName = "BumperStickers";
-        String user = "hna-admin";
-        String password = "HolyNames123";
-        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url);
-            String name;
-            if (post.getName() == null) {
-                name = null;
-            } else {
-                name = String.format("%s", post.getName());
+        //save data from form to database
+        public void saveData(BumperStickerPost post) {
+            String hostName = "hna.database.windows.net";
+            String dbName = "BumperStickers";
+            String user = "hna-admin";
+            String password = "HolyNames123";
+            String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                    + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(url);
+                String name;
+                if (post.getName() == null) {
+                    name = null;
+                } else {
+                    name = String.format("%s", post.getName());
+                }
+                String image;
+                if (post.getImage() == null) {
+                    image = "final bb logo (white).png";
+                } else {
+                    image = String.format("%s", post.getImage());
+                }
+                String title = String.format("%s", post.getTitle());
+                String tags = String.format("%s", post.getTags());
+                String lat = String.format("%f", post.getLat());
+                String lng = String.format("%f", post.getLong());
+                String query = "INSERT INTO post_database(name, image, title, tags, lat, lng) VALUES ('" + name + "','" + image + "','" + title + "','" + tags + "','" + lat + "','" + lng + "')";
+                Statement st = connection.createStatement();
+                st.execute(query);
+            } catch (Exception e) {
+                System.err.println("Got an error! ");
+                System.err.println(e.getMessage());
             }
-            String image;
-            if (post.getImage() == null) {
-                image = "final bb logo (white).png";
-            } else {
-                image = String.format("%s", post.getImage());
-            }
-            String title = String.format("%s", post.getTitle());
-            String tags = String.format("%s", post.getTags());
-            String lat = String.format("%f", post.getLat());
-            String lng = String.format("%f", post.getLong());
-            String query = "INSERT INTO post_database(name, image, title, tags, lat, lng) VALUES ('" + name + "','" + image + "','" + title + "','" + tags + "','" + lat + "','" + lng + "')";
-            Statement st = connection.createStatement();
-            st.execute(query);
-        } catch (Exception e) {
-            System.err.println("Got an error! ");
-            System.err.println(e.getMessage());
         }
+
+
     }
-
-
 }
