5178c85e4fbeef1e0b199b11ec2b0e37549181df
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2021-12-13 07:11:31 -0800

    changes

--- src/main/resources/static/post.js
+++ src/main/resources/static/post.js
@@ -16,23 +16,6 @@ function initMap() {
 
 // this function is executed when the user selects the submit button
   function submitForm(){
-
-    // sets form value with id of name to variable name
-    var name = document.getElementById("name").value;
-
-    // sets form value with id of tags to variable tags
-    var tags = document.getElementById("tags").value;
-
-    // sets form value with id of title to variable title
-    var title = document.getElementById("title").value;
-
-    // sets form value with id of img to variable image
-    var image = document.getElementById("img").value;
-
-    // sets form value with id of latitude to lat
-
-
-    // sets form value of longitude to lng
     window.location.href = "/submit";
     console.log(name);
   }
