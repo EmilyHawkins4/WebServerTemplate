c60666e0c3c969ec71ba1d82b7487681e6c27466
giselaroberts <groberts22@holynames-sea.org>
2022-02-02 07:56:05 -0800

    finished infowindows with images - next: make sure infowindows don't show up all at once

--- src/main/resources/static/content.js
+++ src/main/resources/static/content.js
@@ -3,7 +3,7 @@ let map;
 
 // this function is called in the html every time the page loads
 function initMap() {
-    window.alert(11);
+
   // creating the google map
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 47.62607555, lng: -122.30418194524279 },
@@ -17,11 +17,19 @@ markers.length = postList.length;
     var Post = postList[i];
     var lat = Post.lat;
     var long = Post.long;
+    var title = Post.title;
+    var user = Post.username;
+    var img = Post.image;
 
     markers[i] = new google.maps.Marker({
         position: new google.maps.LatLng(lat, long),
         map: map,
-    });
+        });
+
+        infowindow = new google.maps.InfoWindow({
+                content:"<b>"+ title +"</b><br/> Posted by: " + user +"<br/> <img class = photo src="+ img +" width=200px >" }),
+                google.maps.event.addListener(markers[i], "click", function(){infowindow.open(map,markers[i]);});infowindow.open(map,markers[i]);
+
  }
 
  /* space needle sample marker
