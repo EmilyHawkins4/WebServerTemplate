763b3d41c471a44917a1cd0c97e6c7305fda9cfd
kayla <kmattson22@holynames-sea.org>
2022-02-23 07:16:06 -0800

    image upload

---
+++ src/main/java/service/StorageService.java
@@ -0,0 +1,10 @@
+package service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class StorageService {
+    public String StorageService(MultipartFile file)
+    {
+
+    }
+}
