b733dd7a3c660cea26e6c9a5f57a2adcb79b7a2f
Clare Constantine <clare@code.org>
2022-02-23 07:30:12 -0800

    add azure dependency and imports

--- src/main/java/service/StorageService.java
+++ src/main/java/service/StorageService.java
@@ -1,4 +1,9 @@
 package service;
+package com.azure.storage.file.share;
+
+import com.azure.core.util.Configuration;
+import com.azure.storage.file.share.models.ShareServiceProperties;
+import com.azure.storage.file.share.models.ShareStorageException;
 
 import org.springframework.web.multipart.MultipartFile;
 
