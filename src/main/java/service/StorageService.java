b2e39c4dd1735e55beeca4c0fd4190eb53bdaab4
EMILY HAWKINS <70231187+EmilyHawkins4@users.noreply.github.com>
2022-03-07 07:47:00 -0800

    started to build class

--- src/main/java/service/StorageService.java
+++ src/main/java/service/StorageService.java
@@ -1,15 +1,62 @@
 package service;
-package com.azure.storage.file.share;
 
+import java.io.*;
+import java.util.stream.Stream;
 import com.azure.core.util.Configuration;
-import com.azure.storage.file.share.models.ShareServiceProperties;
-import com.azure.storage.file.share.models.ShareStorageException;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public class StorageService {
-    public String StorageService(MultipartFile file)
+    private static final String CONNECTION_STRING = Configuration.getGlobalConfiguration().get(System.getenv("StorageExplorerConnectionString"));
+    public String StorageService (MultipartFile file)
     {
+        try
+        {
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(CONNECTION_STRING);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+            // Get a reference to a container.
+            // The container name must be lower case
+            CloudBlobContainer container = blobClient.getContainerReference("mycontainer");
+
+            // Create the container if it does not exist.
+            container.createIfNotExists();
+        }
+        catch (Exception e)
+        {
+            // Output the stack trace.
+            e.printStackTrace();
+        }
+
+        try
+        {
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(CONNECTION_STRING);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.getContainerReference("mycontainer");
+
+            CloudBlockBlob blob = container.getBlockBlobReference(file.getName());
+            Stream<String> lines = new BufferedReader(new InputStreamReader(file));
+            blob.upload(file.getInputStream());
+        }
+        catch (Exception e)
+        {
+            // Output the stack trace.
+            e.printStackTrace();
+        }
+
+        return "ok";
+
 
     }
 }
