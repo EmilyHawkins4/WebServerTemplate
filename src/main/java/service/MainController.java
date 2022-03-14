546f5b9e7985756c98039bc3668ec908ec11a32b
kayla <kmattson22@holynames-sea.org>
2022-03-14 07:41:07 -0700

    image upload

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MultipartFile;
+import sun.jvm.hotspot.debugger.ProcessInfo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -50,6 +52,12 @@ public class MainController {
 	public String post(@ModelAttribute BumperStickerPost post, Model model, @RequestParam MultipartFile file){
 		System.out.println("lng: " + post.getLng());
 		System.out.println("lat: " + post.getLat());
+		StorageService f = new StorageService();
+		try {
+			f.StorageServices(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		model.addAttribute("post", post);
 		DatabaseConnect d = new DatabaseConnect();
 		d.saveData(post);
--- src/main/java/service/StorageService.java
+++ src/main/java/service/StorageService.java
@@ -1,8 +1,12 @@
 package service;
 
 import java.io.*;
+import java.util.Map;
 import java.util.stream.Stream;
 import com.azure.core.util.Configuration;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
@@ -10,10 +14,38 @@ import com.microsoft.azure.storage.blob.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public class StorageService {
-    private static final String CONNECTION_STRING = Configuration.getGlobalConfiguration().get(System.getenv("StorageExplorerConnectionString"));
-    public String StorageService (MultipartFile file)
-    {
-        try
+    //private static final String CONNECTION_STRING = Configuration.getGlobalConfiguration().get(System.getenv("storageExplorerConnectionString"));
+    public String StorageServices (MultipartFile file) throws IOException {
+        try {
+            String constr = System.getenv("storageExplorerConnectionString");
+            Map<String, String> env = System.getenv();
+            for (String envName : env.keySet()) {
+                if(envName == "storageExplorerConnectionString"){
+                    constr = env.get(envName);
+                }
+                System.out.format("%s=%s%n", envName, env.get(envName));
+            }
+            System.out.println(constr);
+
+            BlobContainerClient container = new BlobContainerClientBuilder()
+                    .connectionString(constr)
+                    .containerName("upload")
+                    .buildClient();
+
+            BlobClient blob = container.getBlobClient(file.getOriginalFilename());
+
+           // blob.upload(file.getInputStream(), file.getSize(), true);
+            blob.upload(new BufferedInputStream(file.getInputStream()) , file.getInputStream().available());
+            System.out.println("ok!");
+
+        }
+        catch (Exception e){
+            System.err.println("Got an error!");
+            System.err.println(e.getMessage());
+        }
+        return "ok";
+
+       /* try
         {
             // Retrieve storage account from connection-string.
             CloudStorageAccount storageAccount = CloudStorageAccount.parse(CONNECTION_STRING);
@@ -46,8 +78,8 @@ public class StorageService {
             CloudBlobContainer container = blobClient.getContainerReference("mycontainer");
 
             CloudBlockBlob blob = container.getBlockBlobReference(file.getName());
-            Stream<String> lines = new BufferedReader(new InputStreamReader(file));
-            blob.upload(file.getInputStream());
+            //Stream<String> lines = new BufferedReader(new InputStreamReader(file));
+            blob.upload(file.getInputStream(), file.getSize(), true);
         }
         catch (Exception e)
         {
@@ -56,7 +88,7 @@ public class StorageService {
         }
 
         return "ok";
-
+*/
 
     }
 }
